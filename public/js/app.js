66097081e28fc3371f5f24033e031469e4c3b996
Shawn Veader <shawn@veader.org>
2014-02-28 00:53:48 -0500

    now with working filtering

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,28 @@ GrowlerApp.Beer = DS.Model.extend({
 });
 
 GrowlerApp.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return this.store.find('beer');
+	model: function() {
+		return this.store.find('beer');
+	},
+
+	setupController: function(controller, model) {
+    controller.set('model', model);
   }
+});
+
+GrowlerApp.IndexController = Ember.ArrayController.extend({
+  selectedBeerType: null,
+
+  beerTypes: function() {
+    var types = this.mapBy('type');
+    return types.uniq().sort();
+  }.property('@each.type'),
+
+  sortedBeers: function() {
+    if (this.selectedBeerType) {
+      return this.filterBy('type', this.selectedBeerType);
+    } else {
+      return this; // return all the things
+    }
+  }.property('selectedBeerType')
 });
